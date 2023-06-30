c5aff3ef445082ce8318f52d63519c5acc97c771
Vilson Jesuino | Dev <vilsonjesuino.dev@gmail.com>
2023-06-30 20:51:40 -0300

    Innitial commit

---
+++ src/main/java/com/example/socialnetwork/model/AccountInfo.java
@@ -0,0 +1,12 @@
+package com.example.socialnetwork.model;
+
+public class AccountInfo {
+
+    private Long id;
+    private String fullName;
+    private String accountEmail;
+    private String userName;
+    private String phoneNumber;
+    private String country;
+    private String language;
+}
